2270899ea42556e89f3bbc060e5c65b6ddf3e313
Benjamin Aubignat <73789133+OBeeNya@users.noreply.github.com>
2021-05-07 16:48:18 +0200

    Update Assignment.m

--- Week 5/Assignment.m
+++ Week 5/Assignment.m
@@ -10,7 +10,6 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-% Setup some useful variables
 m = size(X, 1);
          
 J = 0;
